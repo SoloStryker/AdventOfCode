0c520f7692d54b387f052fd8f2fe1f0c802f5cb0
SoloStryker <46388630+SoloStryker@users.noreply.github.com>
2019-01-11 22:48:53 -0500

    solution found

--- AoC.c
+++ AoC.c
@@ -7,19 +7,20 @@ int inArray(long val, long *allNums, int size)
 	for (i=0;i<size;i++) //go through all numbers in array
 	{
 		long b=allNums[i]; //set the array num to a var for readability
-		printf("Check No.: %I\n",val);
-		printf("Array Num: %I\n",b);
+		/*
+		printf("Check No.: %ld\n",val);
+		printf("Array Num: %ld\n",b);
 		printf("Array Index: %d\n",i);
 		printf("Size: %d\n",size);
-		printf("Does %I = %I (Array element %I/%d)? \n",val,b,i,size);
+		printf("Does %ld = %ld (Array element %ld/%d)? \n",val,b,i,size); */
 		if (b==val) //compare var to array var
 		{ 
-			printf("Yes\n");
+			printf("Found\n");
 			return 1; //return true if match is found
 		} 
 		else
 		{
-			printf("no\n");
+			//printf("no\n");
 		}
 	}
 	return 0;
@@ -27,8 +28,10 @@ int inArray(long val, long *allNums, int size)
 
 int main()
 {
+	int solved = 0;
 	long baseNum = 0; //initialize the number we'll use to store the count
-	FILE *numFile=fopen("D:\\Projects\\Programming\\input.txt","r"); //Opens input file
+	FILE *numFile = NULL;
+	fopen_s(&numFile,"D:\\Projects\\Programming\\AdventofCode\\input.txt","r"); //Opens input file
 	if (numFile == NULL)//Verifies input file exists, exits if not
 	{
 		printf("Error! opening file");
@@ -36,36 +39,68 @@ int main()
 	}	
 	int total=0; //initializes a counter, to determine the array we will need.
 	long numRead; //initializes int holder for loop
-	while (fscanf(numFile,"%I",&numRead)==1) //Scans file for a value, stores it as numread, iterates through them
+	while (fscanf_s(numFile,"%ld",&numRead)==1) //Scans file for a value, stores it as numread, iterates through them
 	{
-		baseNum+=numRead; //add detected number to counting var
 		total++; //increment counter
 	}	
-	printf("The file has %d numbers\n",total);//a readout
-	long *allNumbers = malloc(total*sizeof(long)); //create array based on size we got (success?)
-	printf("Array is %d",sizeof(allNumbers)/sizeof(long));
+	printf("The file has %d numbers\n",total);
+	long* allTotals = malloc(total*sizeof(long)); //create array for totals based on size we got
+	long* allNumbers = malloc(total * sizeof(long)); //create array of the file nums, size based on size we got
+	printf("Array is %zd element(s)\n\n",_msize(allTotals)/sizeof(long)); //Confirm array completed correctly
 	rewind(numFile); //restart file scanning to beginning
-	/*
-	while (fscanf(numFile,"%I",&numRead)==1) //Scans file again for values, stores them in array
+	int indexNum = 0;
+	baseNum = 0;
+	while (fscanf_s(numFile,"%ld",&numRead)==1) //Scans file again for values, stores them in array
 	{
-		allNumbers+=numRead; //add scanned num to array
-		//printf("Numbers: %d",allNumbers); //console tracking
-		printf("\nScanned %I\n",numRead);
-		if (inArray(numRead,allNumbers,sizeof(allNumbers)/sizeof(long))==1)
+		baseNum += numRead;
+		//printf("Pass 1,Scanned %ld on line %d, added for new total of %ld\n",numRead,indexNum,baseNum);
+		if (inArray(baseNum,allTotals,indexNum)==1)
 		{
-			printf("%d Found twice",numRead); //readout the number found
+			printf("%d Found twice on line %d\n",baseNum,indexNum); //readout the number found
+			solved = 1;
 			break; //stop loop
 		}
-		
-	}	
-	*/
-	
-    printf("after %d lines, The total is %d\n",total,baseNum); //readout
+		allTotals[indexNum] = baseNum; //add new total num to array
+		allNumbers[indexNum] = numRead; //add new total num to array
+		indexNum++;
+	}
 	fclose(numFile); //Close the file
-	free(allNumbers); //Free the memory we allocated
+	//if solution still not found....
+	int pass = 2;
+	indexNum = 0;
+	while (solved == 0)
+	{
+		size_t newSize = _msize(allTotals) + (total * sizeof(long));
+		allTotals= (long *)realloc(allTotals,newSize );
+		for (indexNum = 0; indexNum < total; indexNum++)
+		{
+			baseNum += allNumbers[indexNum];
+			//printf("Pass %d, Scanned %ld on line %d, added for new total of %ld\n", pass, allNumbers[indexNum], indexNum, baseNum);
+			if(inArray(baseNum, allTotals, (int)_msize(allTotals)/sizeof(long))==1)
+			{
+				printf("%d Found twice\n", baseNum); //readout the number found
+				solved = 1;
+				break; //stop loop
+			}
+		}
+		if (solved == 1)
+		{
+			break;
+		}
+		else
+		{
+			pass++;
+			printf("Pass %d, %zd totals checked\n", pass, _msize(allTotals)/sizeof(long));
+			//if (pass > 50) { break; }
+		}
+	}
+
+	printf("after %d lines and %d passes, The total is %d\n", total, pass,baseNum); //readout
 	
-	char ch; //remaining code allows result to be seen in console before window closes
-	printf("Complete");
-	scanf("%c",&ch);
+	free(allTotals); //Free the memory we allocated
+	free(allNumbers);
+	//char ch; //remaining code allows result to be seen in console before window closes
+	printf("Complete\n");
+	//scanf_s("%c",&ch,1);
     return 0;
 }
